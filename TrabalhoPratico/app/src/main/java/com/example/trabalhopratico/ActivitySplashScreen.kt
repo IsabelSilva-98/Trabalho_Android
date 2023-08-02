47eca7685f8c738081949f5028d393d93ba9217e
IsabelSilva-98 <maria.isabel.silva.98@gmail.com>
2023-08-02 17:15:49 +0100

    App_Android

---
+++ TrabalhoPratico/app/src/main/java/com/example/trabalhopratico/ActivitySplashScreen.kt
@@ -0,0 +1,20 @@
+package com.example.trabalhopratico
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class ActivitySplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 4000)
+    }
+}
