2166c06c65b586811a96e8c56feef3ce22387843
IsabelSilva-98 <maria.isabel.silva.98@gmail.com>
2023-08-03 14:49:37 +0100

    Login e registo

--- TrabalhoPratico/app/src/main/java/com/example/trabalhopratico/LoginActivity.kt
+++ TrabalhoPratico/app/src/main/java/com/example/trabalhopratico/LoginActivity.kt
@@ -1,11 +1,24 @@
 package com.example.trabalhopratico
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.trabalhopratico.databinding.ActivityMainBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.buttonLogin.setOnClickListener {  }
+
+        binding.textRegistar.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
+        binding.textRecuperarPass.setOnClickListener {  }
     }
 }
